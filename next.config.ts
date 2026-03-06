96c1c3ba955c598b8c711db103ad485c9d2a21c5
Christoph Bratschi <bratschi@appamics.com>
2026-03-06 17:30:39 +0100

    migrated config to Next.js version

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,9 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   experimental: {
-    swcPlugins: [['@lingui/swc-plugin', {}]],
-    turbo: {
-      rules: {
-        '*.po': {
-          loaders: ['@lingui/loader'],
-          as: '*.js'
-        }
-      }
-    }
+    swcPlugins: [['@lingui/swc-plugin', {}]]
   },
+
   webpack: (config) => {
     config.module.rules.push({
       test: /\.po$/,
@@ -22,6 +15,15 @@ const nextConfig: NextConfig = {
 
     return config;
   },
+
+  turbopack: {
+    rules: {
+      '*.po': {
+        loaders: ['@lingui/loader'],
+        as: '*.js'
+      }
+    }
+  }
 }
 
 export default nextConfig
